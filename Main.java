3a5fa5836867f7613421a2fd0ac2e33f2c860ff8
Téo <76852029+tetemh@users.noreply.github.com>
2022-07-19 21:22:49 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,16 @@
-package eu.tetemh.jobs;
+package eu.tetemh.exemple;
 
 import eu.tetemh.jobs.bdd.SqlConnection;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
-    private static Main instance;
     private static SqlConnection sql;
 
     @Override
     public void onEnable() {
-        sql = new SqlConnection("jdbc:mysql://", "87.106.169.47:3309", "s101_survival", "", "");
+        sql = new SqlConnection("jdbc:mysql://", "host", "db_name", "name", "password");
         sql.connection();
-        Main.instance = this;
     }
 
     @Override
@@ -22,8 +20,5 @@ public final class Main extends JavaPlugin {
 
     public static SqlConnection getSqlConnection() {
         return sql;
-    }
-    public static Main getInstance() {
-        return instance;
     }
 }
